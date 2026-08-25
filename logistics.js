771b4c2ee2f29807d6b77ef06251034c5ef5a3c9
matthieu.c <300508217+matchape01@users.noreply.github.com>
2026-08-26 01:29:14 +0200

    [Tantramour 2026] Mise à jour logistics.js

--- logistics.js
+++ logistics.js
@@ -91,7 +91,7 @@ var LOGISTICS = {
     text: "",
     validatedByFac: false,
     prepDuration: 30,
-    updatedAt: "2026-08-25 01:27"
+    updatedAt: "2026-08-25 01:29"
   },
 
   "JOUR2_INMEN___HELD_BY_MEN_A_SOMATIC_": {
