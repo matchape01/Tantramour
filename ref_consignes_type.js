7808fd7cfa99e092e2dbe8d4565ace6f5a708030
matthieu.c <300508217+matchape01@users.noreply.github.com>
2026-08-26 15:10:10 +0200

    [Tantramour 2026] Mise à jour ref_consignes_type.js

--- ref_consignes_type.js
+++ ref_consignes_type.js
@@ -32,7 +32,7 @@ var REF_CONSIGNES_TYPE = [
     descriptif: "",
     placements: ["Avant","Après"],
     actif: false,
-    types: ["MEDITATION / YOGA","ATELIERS TANTRA (Matin)","ATELIERS TANTRA (Apres-Midi)","ATELIER ARTISTIQUE (Matin)","ATELIER ARTISTIQUE (Apres-Midi)","LOVE TEMPLE"]
+    types: ["ATELIERS TANTRA (Matin)","ATELIERS TANTRA (Apres-Midi)","ATELIER ARTISTIQUE (Matin)","ATELIER ARTISTIQUE (Apres-Midi)","LOVE TEMPLE","MEDITATION / YOGA"]
   },
   {
     id: "CONS_NEW003",
