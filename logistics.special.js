522f345e60d0fa04e0071894f0b4452f49cdbd99
matthieu.c <300508217+matchape01@users.noreply.github.com>
2026-08-26 22:10:42 +0200

    [Tantramour 2026] Mise à jour logistics.special.js

--- logistics.special.js
+++ logistics.special.js
@@ -633,7 +633,7 @@ var LOGISTICS_SPECIAL = {
   "JOUR4_BALANCE_CONCERT_KELLY_AURA___C": {
     html: "<div>Preparer peut etre un autel, fleurs bougies devant nous pour le concert&nbsp;</div><div>Prevoir de l'eau pour nous pendant le concert&nbsp;</div>",
     text: "Preparer peut etre un autel, fleurs bougies devant nous pour le concert \nPrevoir de l'eau pour nous pendant le concert",
-    updatedAt: "2026-08-26 22:06"
+    updatedAt: "2026-08-26 22:10"
   }
 
 };
