b22a3cd9e29d32f7f938cc053eeefaff3a6e8f79
matthieu.c <300508217+matchape01@users.noreply.github.com>
2026-08-26 21:46:28 +0200

    [Tantramour 2026] Mise à jour logistics.helpers.js

--- logistics.helpers.js
+++ logistics.helpers.js
@@ -296,7 +296,7 @@ var LOGISTICS_HELPERS = {
     avant: "",
     pendant: "",
     apres: "",
-    updatedAt: "2026-08-26 21:44"
+    updatedAt: "2026-08-26 21:46"
   },
 
   "JOUR4_DANSE_FUSION___GUIDER___SE_LAI": {
