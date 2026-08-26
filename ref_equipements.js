1ba689ecf0478683a47878c1b89358fa4d93ce70
matthieu.c <300508217+matchape01@users.noreply.github.com>
2026-08-26 21:53:56 +0200

    [Tantramour 2026] Mise à jour ref_equipements.js

--- ref_equipements.js
+++ ref_equipements.js
@@ -67,5 +67,6 @@ var REF_EQUIPEMENTS = [
   { type: "Tantramour", categorie: "Matériel de Pratique", id: "EQ_NEW03", value: "Masque", stock: "N/A", description: "Masque", remarque: "" },
   { type: "Hameau", categorie: "Technique & Son", id: "EQ_TABMIX", value: "Table de mixage", stock: "N/A", description: "Table de mixage", remarque: "" },
   { type: "Hameau", categorie: "Technique & Son", id: "EQ_SONO", value: "Sono", stock: "N/A", description: "Sono", remarque: "" },
-  { type: "Hameau", categorie: "Technique & Son", id: "EQ_NEW04", value: "Stand de guitare", stock: 0, description: "", remarque: "" }
+  { type: "Hameau", categorie: "Technique & Son", id: "EQ_NEW04", value: "Stand de guitare", stock: 0, description: "", remarque: "" },
+  { type: "Hameau", categorie: "Technique & Son", id: "EQ_NEW05", value: "Câble connexion guitare", stock: 0, description: "", remarque: "" }
 ];
