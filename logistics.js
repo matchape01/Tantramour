972aae135651631314e02ee63eba1266b2f32771
matthieu.c <300508217+matchape01@users.noreply.github.com>
2026-08-26 21:44:12 +0200

    [Tantramour 2026] Mise à jour logistics.js

--- logistics.js
+++ logistics.js
@@ -440,10 +440,10 @@ var LOGISTICS = {
 
   "JOUR4_DANSE_ECSTATIC_CONTACT__2_": {
     html: "Système son <br>3 pieds de micro <br>3 micro",
-    text: "Système son \n3 pieds de micro \n3 micro",
+    text: "Système son\n3 pieds de micro\n3 micro",
     validatedByFac: false,
     prepDuration: 30,
-    updatedAt: "2026-08-25 03:37"
+    updatedAt: "2026-08-26 21:44"
   },
 
   "JOUR4_DANSE_FUSION___GUIDER___SE_LAI": {
