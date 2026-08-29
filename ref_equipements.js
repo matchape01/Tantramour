e33a6b2115fe7c297bfec066c614cc992cc617c3
matthieu.c <300508217+matchape01@users.noreply.github.com>
2026-08-29 12:59:13 +0200

    [Tantramour 2026] Mise à jour ref_equipements.js

--- ref_equipements.js
+++ ref_equipements.js
@@ -91,4 +91,5 @@ var REF_EQUIPEMENTS = [
   { type: "Tantramour", categorie: "Décoration & Rituel", id: "EQ_TAPSOL", value: "Tapis de sol décoratif", stock: 0, description: "Tapis de sol décoratif", remarque: "" },
   { type: "Tantramour", categorie: "Fournitures", id: "EQ_WATER", value: "Bouteilles d'eau", stock: 0, description: "Bouteilles d'eau", remarque: "" },
   { type: "Hameau", categorie: "Matériel de Pratique", id: "EQ_ZAFU", value: "Zafu animateur", stock: 32, description: "Zafu animateur", remarque: "" },
+  { type: "Tantramour", categorie: "Décoration & Rituel", id: "EQ_NEW04", value: "Petites fleurs séchées", stock: 0, description: "pour autel Shiva", remarque: "" }
 ];
