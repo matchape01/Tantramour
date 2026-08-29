7da2719177879a9bb9974c280449e84189d4099f
matthieu.c <300508217+matchape01@users.noreply.github.com>
2026-08-29 16:31:43 +0200

    [Tantramour 2026] Mise à jour logistics.helpers.js

--- logistics.helpers.js
+++ logistics.helpers.js
@@ -766,6 +766,13 @@ var LOGISTICS_HELPERS = {
     pendant: "",
     apres: "",
     updatedAt: "2026-08-28 20:15"
+  },
+
+  "JOUR2_PREPA_TEMPLE__CHENREZIG____SHI": {
+    avant: "Mise en place de place assise pour la perf (je serai la pour guider comment) /<br><br>60 yoga mat (chin mudra) pour la jam /<br><br>Lights pro pour la décoration de la salle. <br>3 pour la scène + quelque autre pour lumière d'ambiance salle.<br><br>Point à gérer par l'équipe en charge.",
+    pendant: "",
+    apres: "",
+    updatedAt: "2026-08-29 16:31"
   }
 
 };
