f522db31e76c55a1841243c6570f900673286882
matthieu.c <300508217+matchape01@users.noreply.github.com>
2026-08-29 14:36:38 +0200

    [Tantramour 2026] Mise à jour logistics.helpers.js

--- logistics.helpers.js
+++ logistics.helpers.js
@@ -9,7 +9,7 @@ var LOGISTICS_HELPERS = {
     avant: "<span style=\"font-family: Calibri; font-size: 14.6667px;\">• </span>15 mins avant 18h : faire venir festivaliers pour se mettre sur le chemin devant l'entrée SHIVA (et permettre ainsi une installation fluide et optimisée)",
     pendant: "<div><span style=\"font-family: Calibri; font-size: 11pt;\">•&nbsp;</span>Accueil à l'entrée et proposer la corbeille pour choix de la fleur</div><div><span style=\"font-family: Calibri; font-size: 14.6667px;\">• </span>Une fois tout le monde arrivé : Helpers se positionnent en contenant au bord de Shiva&nbsp;</div><div><span style=\"font-family: Calibri; font-size: 14.6667px;\">• </span>Phase où tous et toutes se lèvent : Helpers récupèrent les coussins pour mettre sur le côté</div><div><span style=\"font-family: Calibri; font-size: 14.6667px;\">• </span>Pour la fluidité. : si un.e participant.e est seul, faire la pratique avec lui.elle.</div><div><br></div>",
     apres: "<div><span style=\"font-family: Calibri; font-size: 11pt;\">•&nbsp;</span>Message aimable pour les personnes qui tardent à aller au restaurant : \" c'est l'heure de se diriger vers le restau (reprise à 21h !) donc ne pas trainer !\"</div>",
-    updatedAt: "2026-08-29 13:19"
+    updatedAt: "2026-08-29 14:36"
   },
 
   "JOUR1_SOIREE_ECSTATIC_DANSE": {
