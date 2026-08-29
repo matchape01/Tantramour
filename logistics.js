7a9e4cc15eec422157acfd5ccd08adf0202a8011
matthieu.c <300508217+matchape01@users.noreply.github.com>
2026-08-29 14:36:38 +0200

    [Tantramour 2026] Mise à jour logistics.js

--- logistics.js
+++ logistics.js
@@ -27,7 +27,7 @@ var LOGISTICS = {
     text: "- 250 petites fleurs de la nature (séchées ou pas) - Delphine essaye d'en trouver sur sa route mais si quelqu'un.e peut s'en occuper le samedi matin (nature du Hameau, chemin vers la grotte), pour compléter, c'est bien-venu. S'il y en a trop, ça servira pour autour de l'autel.\n- 4 à 5 contenants pour les y mettre\n- Préparer un autel au centre de SHIVA (statu à identifier) avec coussins en spirale qui partent du centre\n- Autant de coussins que de participants + personnes de l'équipe (sauf helpers qui restent au bord pour 'laccueil puis le contenant)\n- préparer une joli déco (fleurs, lumières à LED,...) sur le chemin qui mène à SHIVA\n\n\nLES INFORMATIONS COMPLEMENTAIRES SERONT COMMUNIQUÉES LORS DE LA RÉUNION DU MATIN.",
     validatedByFac: false,
     prepDuration: 30,
-    updatedAt: "2026-08-29 13:19"
+    updatedAt: "2026-08-29 14:36"
   },
 
   "JOUR1_REUNION_D_EQUIPE__HELPERS_": {
