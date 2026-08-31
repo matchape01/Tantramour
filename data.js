f1ec79cd3fa77ea42bb066bae4d0a9f512cae3bc
matthieu.c <300508217+matchape01@users.noreply.github.com>
2026-08-31 12:54:48 +0200

    [Tantramour 2026] Mise à jour data.js

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-// @saved:2026-08-31 12:53:41
+// @saved:2026-08-31 12:54:47
 /**
  * TANTRAMOUR 2026 — Référentiel de données commun
  * ================================================
@@ -350,6 +350,7 @@ var AGENDA = [
 
   // ─── Jour 5 — Mercredi 2 septembre ──────────────────────────────────
   {id:"A_1245",jour:"Jour 5",date:"Mercredi 2 septembre",heure:"23:30 - 01:00",type:"PREPA & LOGISTICS",atelier:"EXTRA SUPPORT - Temple du Mystère et de la Fantaisie",lieu:"CHENREZIG",fac1:"Vera De Sousa",fac2:"",fac3:"",fac4:"",traduction:"",helper1:"Selma (Céline Laroche)",helper2:"Damien Eissen",helper3:"",helper4:"",angel:"Daniel Latapie",note:"",piment:2,colibri:false,logisticId:"",meetingRoles:""},
+  {id:"A_4472",jour:"Jour 5",date:"Mercredi 2 septembre",heure:"20:00 - 22:00",type:"PREPA & LOGISTICS",atelier:"PREPA TEMPLE (CHENREZIG) - Le mystere (Jivan & Le Splendid) (COPIE)",lieu:"CHENREZIG",fac1:"Jivan Muti (Clément Victor)",fac2:"Atman Clochette (Matthieu)",fac3:"Vera De Sousa",fac4:"",traduction:"",helper1:"Maeva Mantione",helper2:"Delphine Dupré",helper3:"Yannick Bohrer",helper4:"Linda Stachetti",angel:"",note:"",piment:0,colibri:false,logisticId:"",meetingRoles:""},
 ];
 
 /**
