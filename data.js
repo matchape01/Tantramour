dc8ec65f8d63e00303e2bb88475db77d91353871
matthieu.c <300508217+matchape01@users.noreply.github.com>
2026-08-31 09:02:38 +0200

    [Tantramour 2026] Mise à jour data.js

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-// @saved:2026-08-31 08:59:37
+// @saved:2026-08-31 09:02:36
 /**
  * TANTRAMOUR 2026 — Référentiel de données commun
  * ================================================
@@ -200,7 +200,7 @@ var AGENDA = [
 
   // ─── Jour 6 — Jeudi 3 septembre ─────────────────────────────────────
   {id:"A_1132",jour:"Jour 6",date:"Jeudi 3 septembre",heure:"7:30 - 9:00",type:"MEDITATION / YOGA",atelier:"AcroYoga ",lieu:"CHENREZIG",fac1:"Sevda Duroy",fac2:"ShivaChris",fac3:"",fac4:"",traduction:"Yannick Bohrer",helper1:"",helper2:"",helper3:"",helper4:"",angel:"",note:"",piment:1,colibri:false,logisticId:"LOG_Jour6_ACROYOGA_",meetingRoles:""},
-  {id:"A_1133",jour:"Jour 6",date:"Jeudi 3 septembre",heure:"7:30 - 9:00",type:"MEDITATION / YOGA",atelier:"Méditation & Mouvements Somatiques",lieu:"SHAKTI",fac1:"Linda Stachetti",fac2:"",fac3:"",fac4:"",traduction:"",helper1:"Philippe Hanrion",helper2:"",helper3:"",helper4:"",angel:"",note:"",piment:1,colibri:false,logisticId:"LOG_Jour6_MINDFULNESS___MOUVEMENTS_SOMAT",meetingRoles:""},
+  {id:"A_1133",jour:"Jour 6",date:"Jeudi 3 septembre",heure:"7:30 - 9:00",type:"MEDITATION / YOGA",atelier:"Méditation & Mouvements Somatiques",lieu:"SHAKTI",fac1:"Linda Stachetti",fac2:"",fac3:"",fac4:"",traduction:"",helper1:"Véronique Santini Bottemer",helper2:"",helper3:"",helper4:"",angel:"",note:"",piment:1,colibri:false,logisticId:"LOG_Jour6_MINDFULNESS___MOUVEMENTS_SOMAT",meetingRoles:""},
   {id:"A_0175",jour:"Jour 6",date:"Jeudi 3 septembre",heure:"7:30 - 10:00",type:"REPAS & PAUSE",atelier:"PETIT DEJ",lieu:"",fac1:"",fac2:"",fac3:"",fac4:"",traduction:"",helper1:"",helper2:"",helper3:"",helper4:"",angel:"",note:"",piment:0,colibri:false,logisticId:"",meetingRoles:""},
   {id:"A_8951",jour:"Jour 6",date:"Jeudi 3 septembre",heure:"9:00 - 9:30",type:"PREPA & LOGISTICS",atelier:"PREPARATION Rassemblement",lieu:"SHIVA",fac1:"",fac2:"",fac3:"",fac4:"",traduction:"",helper1:"Frédéric Chalard",helper2:"Damien Eissen",helper3:"",helper4:"",angel:"",note:"",piment:0,colibri:false,logisticId:"JOUR6_PREPARATION_RASSEMBLEMENT",meetingRoles:""},
   {id:"A_7088",jour:"Jour 6",date:"Jeudi 3 septembre",heure:"9:45 - 11:00",type:"RASSEMBLEMENT",atelier:"Rassemblement du matin",lieu:"SHIVA",fac1:"Jivan Muti (Clément Victor)",fac2:"Atman Clochette (Matthieu)",fac3:"",fac4:"",traduction:"Amana",helper1:"Vera De Sousa",helper2:"Linda Stachetti",helper3:"",helper4:"",angel:"",note:"",piment:0,colibri:false,logisticId:"LOG_Jour6_RASSEMBLEMENT",meetingRoles:""},
